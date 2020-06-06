3976a497c0a8fb634badff40c49c043e3ac60e70
himanshu20005 <himanshuparvanda@gmail.com>
2020-06-06 18:30:21 +0530

    Initial Commit

---
+++ assets/js/counter.js
@@ -0,0 +1,36 @@
+// $(".counter-count").each(function () {
+//   $(this)
+//     .prop("Counter", 0)
+//     .animate(
+//       {
+//         Counter: $(this).text(),
+//       },
+//       {
+//         duration: 5000,
+//         easing: "swing",
+//         step: function (now) {
+//           $(this).text(Math.ceil(now));
+//         },
+//       }
+//     );
+// });
+
+// Counter
+$(document).ready(function () {
+  $(".counter").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
+});
